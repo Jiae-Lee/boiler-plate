2239fc311a095aad8370f11d746c4975c1931a84
jiae lee <leejiae@vtstechlab.com>
2021-06-28 17:52:29 +0900

    폴더 분리, Route

---
+++ client/src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link 
+} from 'react-router-dom';
+import Home from '../Home';
+import Menu from '../service/Menu'
+import LandingPage from './views/LandingPage/LandingPage'
+import LoginPage from './views/LoginPage/LoginPage'
+import RegisterPage from './views/RegisterPage/RegisterPage'
+
+function App() {
+  return (
+    <div>
+      <Route exact path="/" component={ Home } />
+      <Route path="/menu" component={ Menu } />
+      <Route path="/landing" component={ LandingPage } />
+      <Route path="/login" component={ LoginPage } />
+      <Route path="/register" component={ RegisterPage } />
+    </div>
+  );
+}
+
+export default App;
